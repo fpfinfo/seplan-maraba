5e4215d7d092a49443e6c2b0d7164af081d0e303
fpfinfo <fpf.info@gmail.com>
2025-06-25 12:04:45 -0300

    atualizar

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,14 @@
-const CACHE_NAME = "estudo-tecnico-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
+const CACHE_NAME = "estudo-tjpa-cache-v1";
+const urlsToCache = ["index.html", "manifest.json", "service-worker.js"];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
